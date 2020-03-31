0629a4287d8b3a606722c69df7009cc8da583715
hyunchul-joo <canij1987@gmail.com>
2020-03-31 14:45:06 +0900

    해결

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://joo:8377@boilerplate-gkr7y.mongodb.net/test?retryWrites=true&w=majority', {
+	useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
